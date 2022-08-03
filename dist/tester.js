b3c6bc264d6095369c4fab9f11560b6d0ca1e8d8
oscargelin <oscargelin@gmail.com>
2022-08-03 16:19:57 +0200

    Added array with strings

---
+++ dist/tester.js
@@ -0,0 +1,3 @@
+"use strict";
+let temp = "Test";
+let strings = ["En", "liten", "string", "array"];
---
+++ src/tester.ts
@@ -0,0 +1,3 @@
+let temp: string = "Test";
+
+let strings: string[] = ["En", "liten", "string", "array"];
