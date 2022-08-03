5ddae943df21ab002b203fd7fdbc14952c297b0c
oscargelin <oscargelin@gmail.com>
2022-08-03 17:41:03 +0200

    Added another feature

--- src/newFeature.ts
+++ src/newFeature.ts
@@ -1,5 +1,5 @@
 let newFeature: string = "I am a new feature";
 let anotherFeature: string = "This is another feature";
+let featureTwo: number = 2;
 let aThirdFeature: boolean = true;
 console.log(`${newFeature} and im cool`);
-let conflict = false;
