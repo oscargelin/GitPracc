17b9251958f73ff8e623dfcb3082ff24cade7581
oscargelin <oscargelin@gmail.com>
2022-08-03 17:45:41 +0200

    new change

--- src/newFeature.ts
+++ src/newFeature.ts
@@ -3,3 +3,4 @@ let anotherFeature: string = "This is another feature";
 let featureTwo: number = 2;
 let aThirdFeature: boolean = true;
 console.log(`${newFeature} and im cool`);
+console.log("123");
