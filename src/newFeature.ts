bfd5bf3bd1b763cb8332ca35f36274b0f44a70f2
oscargelin <oscargelin@gmail.com>
2022-08-03 16:50:09 +0200

    conflict two

--- src/newFeature.ts
+++ src/newFeature.ts
@@ -2,3 +2,4 @@ let newFeature: string = "I am a new feature";
 let anotherFeature: string = "This is another feature";
 let aThirdFeature: boolean = true;
 console.log(`${newFeature} and im cool`);
+let conflict = false;
